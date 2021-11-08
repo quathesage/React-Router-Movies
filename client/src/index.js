3a3ef0a30af1b3d5cd1dae5f3795e122b6a1891c
Qua Wilburn <quawilburn@Quas-MacBook-Air.local>
2021-11-08 17:11:40 -0600

    complete

--- client/src/index.js
+++ client/src/index.js
@@ -10,5 +10,5 @@ import { BrowserRouter as Router } from 'react-router-dom'
 ReactDOM.render(
  <Router>
     <App />
- </Router>,
-document.getElementById('root'));
+ </Router>
+ ,document.getElementById('root'));
